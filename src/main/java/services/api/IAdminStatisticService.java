4b37655c461bbe47c119968a8d563c636fabea08
Artur <sharam.artur@gmail.com>
2022-12-20 00:32:54 +0300

    created IAdminStatisticService and ListenerOnlineUsers

---
+++ src/main/java/services/api/IAdminStatisticService.java
@@ -0,0 +1,11 @@
+package services.api;
+
+public interface IAdminStatisticService {
+    String getStatistic();
+
+    int getMessageStatistic();
+
+    String getOnlineUsers();
+
+    String getUserStatistic();
+}
