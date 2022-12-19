101f0e09dd6fa6d9a6dd846f75f403f40ffe73ae
Artur <sharam.artur@gmail.com>
2022-12-20 00:51:19 +0300

    created AdminStatisticService and updated ServiceProvider

---
+++ src/main/java/services/AdminStatisticService.java
@@ -0,0 +1,50 @@
+package services;
+
+import dao.api.IMessageDAO;
+import dao.api.IUserDAO;
+import listeners.ListenerOnlineUsers;
+import services.api.IAdminStatisticService;
+
+public class AdminStatisticService implements IAdminStatisticService {
+    private final IUserDAO userDAO;
+    private final IMessageDAO messageDAO;
+
+    public AdminStatisticService(IUserDAO userDAO, IMessageDAO messageDAO) {
+        this.userDAO = userDAO;
+        this.messageDAO = messageDAO;
+    }
+
+    @Override
+    public String getStatistic() {
+        StringBuilder sb=new StringBuilder();
+        sb.append("The number of messages sent in the application: ")
+                .append(getMessageStatistic()).append("\t")
+                .append("Online users: ").append("\t")
+                .append(getOnlineUsers()).append("\t")
+                .append("Registered users in the application")
+                .append("\t")
+                .append(getUserStatistic());
+        return sb.toString();
+    }
+
+    @Override
+    public int getMessageStatistic() {
+        return messageDAO.getAll().size();
+    }
+
+    @Override
+    public String getOnlineUsers() {
+        StringBuilder sb = new StringBuilder();
+        ListenerOnlineUsers.getInstance().getOnlineUsers().
+                forEach(user -> sb.append(user.getLogin()).append("\n"));
+        return sb.toString();
+    }
+
+    @Override
+    public String getUserStatistic() {
+        StringBuilder sb = new StringBuilder();
+        userDAO.getAll().forEach(user -> sb.append(user.getLogin()).append("\n"));
+        return sb.toString();
+    }
+}
+
