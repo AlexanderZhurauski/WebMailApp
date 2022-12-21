9b141c8e234c51b24922f6bc573cc81130cd3768
Dmitry Kulinenko <kulinenko34@gmail.com>
2022-12-21 18:48:05 +0300

    added LoginService and LoginServlet

---
+++ src/main/java/controllers/servlets/LoginServlet.java
@@ -0,0 +1,59 @@
+package controllers.servlets;
+
+import dto.UserLoginDTO;
+import dto.UserSessionDTO;
+import services.ServiceProvider;
+import services.api.ILoginService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "api/login")
+public class LoginServlet extends HttpServlet {
+    private final ILoginService loginService;
+    private final String LOGIN_PARAM_NAME = "login";
+    private final String PASSWORD_PARAM_NAME = "password";
+
+    public LoginServlet() {
+        this.loginService = ServiceProvider.getInstance().getLoginService();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+
+        String login = getRequestParam(req, LOGIN_PARAM_NAME);
+        String password = getRequestParam(req, PASSWORD_PARAM_NAME);
+        loginService.singIn(new UserLoginDTO(login, password));
+
+        HttpSession session = req.getSession();
+        session.setAttribute(login, loginService.getUserSessionDTO(login));
+        writer.append("<h2>login completed successfully</h2>");
+    }
+
+    private String getRequestParam(HttpServletRequest req, String name) {
+        String[] param = req.getParameterValues(name);
+        if (param == null || param.length == 0) {
+            throw new IllegalArgumentException("User failed to provide a " +
+                    name);
+        }
+        if (param.length > 1) {
+            throw new IllegalArgumentException("More than one " + name +
+                    " has been provided");
+        }
+        if (param[0] == null || param[0].isBlank()) {
+            throw new IllegalArgumentException("Invalid " + name +
+                    " provided");
+        }
+
+        return param[0];
+    }
+}
