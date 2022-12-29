fcd90859b608a5ec2bc9a1e80adbab86e78be8ad
MaxStribuk <maximus_747@mail.ru>
2022-12-29 14:12:17 +0300

    refactoring

--- src/main/java/controllers/servlets/LoginServlet.java
+++ src/main/java/controllers/servlets/LoginServlet.java
@@ -4,6 +4,7 @@ import dto.UserLoginDTO;
 import services.ServiceProvider;
 import services.api.ILoginService;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,7 +24,8 @@ public class LoginServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter writer = resp.getWriter();
--- src/main/java/controllers/servlets/MessageServlet.java
+++ src/main/java/controllers/servlets/MessageServlet.java
@@ -5,6 +5,7 @@ import dto.UserSessionDTO;
 import services.ServiceProvider;
 import services.api.IMessageService;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,7 +24,7 @@ public class MessageServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+            throws ServletException, IOException {
 
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
@@ -49,7 +50,7 @@ public class MessageServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+            throws ServletException, IOException {
 
         req.setCharacterEncoding("UTF-8");
         HttpSession currentSession = req.getSession();
