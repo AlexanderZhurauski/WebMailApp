314c915beb601b39e897405fe1cedba7564ca692
Artur <sharam.artur@gmail.com>
2023-01-03 14:25:13 +0300

    add filter for admins, selrvlet for adminstatistic.jsp and adminstatistic.jsp

---
+++ src/main/java/controllers/filters/AdminSecurityFilter.java
@@ -0,0 +1,39 @@
+package controllers.filters;
+
+import dto.UserRole;
+import dto.UserSessionDTO;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebFilter(urlPatterns = {"/ui/admin/*", "/api/admin/*"})
+public class AdminSecurityFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String contextPath = req.getContextPath();
+        HttpSession session = req.getSession();
+        if ((session != null) && (session.getAttribute("user") != null) &&
+                ((UserSessionDTO) session.getAttribute("user")).getRole() == UserRole.ADMIN) {
+            chain.doFilter(request, response);
+        } else {
+            resp.sendRedirect(contextPath+"/ui/singIn");
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
